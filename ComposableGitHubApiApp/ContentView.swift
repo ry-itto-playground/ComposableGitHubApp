40ce65b9ce25223d7c61c263c4477f229e54c547
ryoya ito <110ryoya@gmail.com>
2020-05-31 10:17:52 +0900

    :tada: init

---
+++ ComposableGitHubApiApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ComposableGitHubApiApp
+//
+//  Created by 伊藤凌也 on 2020/05/31.
+//  Copyright © 2020 ry-itto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
